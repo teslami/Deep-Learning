4337f36ac2df97ece393414259cd81d2c5354bcb
teslami <teslamy68@gmail.com>
2024-10-06 00:32:41 -0500

    add multihead attention

--- LLM/transformers/attention_function.py
+++ LLM/transformers/attention_function.py
@@ -2,6 +2,25 @@ import torch
 import numpy as np
 from torch.nn import Softmax
 
+def multihead_attention(num_head, q, k, v):
+    '''
+    performing multihead attention
+    num_head: number of heads
+    embeddings: tensor of size bxnxd
+        b: batch size
+        n: number of tokens in each bactch
+        d: embedding size
+        q, k, v: query, key, value
+    '''
+    print(q.size())
+    d = q.shape[1]
+    head_dim = int(d/num_head)
+    q = torch.split(q, head_dim, dim=1)[0]
+    k = torch.split(k, head_dim, dim=1)[0]
+    v = torch.split(v, head_dim, dim=1)[0]
+    for i in range(num_head):
+        attetion_head = custom_sdp_attention(q, k, v)
+
 
 def custom_sdp_attention(q, k, v):
     '''
@@ -13,18 +32,19 @@ def custom_sdp_attention(q, k, v):
     output: attention value a: nxdv
     '''
     d = q.shape[-1]
-    s = Softmax(dim=1)
-    print(s(torch.matmul(q, k.T)/np.sqrt(d)))
-    self_attention = torch.matmul(s(torch.matmul(q, k.T)/np.sqrt(d)), v)
+    s = Softmax(dim=2)
+    self_attention = torch.matmul(s(torch.matmul(q, torch.transpose(k, 1, 2))/np.sqrt(d)), v)
     return self_attention
 
 
-n = 4
+batch_size = 2
+n = 18  # number of sequences
 dk = 8
 dv = 10
-q = torch.rand(n, dk)
-k = torch.rand(n, dk)
-v = torch.rand(n, dv)
-attention_custom = custom_sdp_attention(q, k, v)
-attention_pytorch = torch.nn.functional.scaled_dot_product_attention(q, k, v)
-print(torch.allclose(attention_custom, attention_pytorch))
+q = torch.rand(batch_size, n, dk)
+k = torch.rand(batch_size, n, dk)
+v = torch.rand(batch_size, n, dv)
+# attention_custom = custom_sdp_attention(q, k, v)
+# attention_pytorch = torch.nn.functional.scaled_dot_product_attention(q, k, v)
+# print(torch.allclose(attention_custom, attention_pytorch))
+multihead_attention(3, q, k, v)
