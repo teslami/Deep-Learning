f9d3c0dc50ab4e16ad093cef6afd089961df35cc
teslami <teslamy68@gmail.com>
2024-10-08 00:37:32 -0500

    add layernorm in transfomer encoder

--- LLM/transformers/transformr_encoder.py
+++ LLM/transformers/transformr_encoder.py
@@ -1,40 +1,46 @@
 import torch
 import attention_function
-from torch.nn import Linear
+from torch.nn import Linear, LayerNorm
 from positional_encoding import positional_encoding
 
 
-class encoder(torch.nn.Module):
-    def __init__(self, num_att_block, token_embedding, dim_hidden_linear):
+class multi_head_attention_block(torch.nn.Module):
+    def __init__(self, num_att_block, token_embedding, dim_hidden_linear, feedforward_dim):
         '''
         token_embedding: token embeddings of size bxnxd
             b: batch size
             n: token size
             d: embedding dim
         '''
-        super(encoder, self).__init__()
+        super(multi_head_attention_block, self).__init__()
         self.token_embedding = token_embedding
         self.num_att_block = num_att_block
         self.embedding_dim = token_embedding.shape[2]
-        self.dim_per_head = int(self.embedding_dim/self.num_att_block)
         self.num_tokens = token_embedding.shape[1]
         self.linear_q = Linear(self.embedding_dim, dim_hidden_linear)
         self.linear_k = Linear(self.embedding_dim, dim_hidden_linear)
         self.linear_v = Linear(self.embedding_dim, dim_hidden_linear)
+        self.layer_norm = LayerNorm(normalized_shape=self.embedding_dim)
+        self.feedforward_linear1 = Linear(self.embedding_dim, feedforward_dim)
+        self.feedforward_linear2 = Linear(feedforward_dim, self.embedding_dim)
 
     def forward(self):
         '''
-        embedding of size b, n, m
+        performs multihead attention on embedding of size b, n, m
             b: batch size
             n: number of tokens
             m: embedding length
+
+        computing positional encoding, adding it to toekn embeddings,
+        applying multihead attention
+
+        returns new computed embedding
         '''
         embedding = self.token_embedding
         pos_encoding = positional_encoding(self.num_tokens, self.embedding_dim)
         pos_encoding = pos_encoding.expand(embedding.shape[0], -1, -1)  # duplicating this for all token embeddings
-        print(embedding.shape)
         embedding = embedding+pos_encoding  # size: bxnxd
-        embedding = torch.tensor(embedding, dtype=torch.float32)
+        embedding = self.layer_norm(embedding.float())
         #  generating vectors q, k and v using three different linear transformations
         q = self.linear_q(embedding)
         k = self.linear_k(embedding)
@@ -42,9 +48,10 @@ class encoder(torch.nn.Module):
         output_multihead_attention = attention_function.multihead_attention(
             self.num_att_block,
             q, k, v)
-        print(output_multihead_attention.shape)
+        return output_multihead_attention
 
 
-transformer_encode = encoder(num_att_block=3, token_embedding=torch.rand(2, 30, 256), dim_hidden_linear=512)
-
-transformer_encode()
+transformer_encode = multi_head_attention_block(
+    num_att_block=3, token_embedding=torch.rand(2, 30, 256),
+    dim_hidden_linear=512, feedforward_dim=1024)
+output_embeddings = transformer_encode()
